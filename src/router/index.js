2bc6c993400804dc8a2086a48e3bbbce7e445e22
michal-grabowski <michal.grabowski@schibsted.com>
2022-01-31 07:29:11 +0100

    Add hash history

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Workspace from '../components/Workspace.vue';
 import JoinRoom from '../views/JoinRoom.vue';
 import PokerRoom from '../views/PokerRoom.vue';
@@ -16,8 +16,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  mode: 'hash',
+  history: createWebHashHistory(process.env.BASE_URL),
   routes,
 });
 
